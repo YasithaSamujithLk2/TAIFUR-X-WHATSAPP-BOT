00ac71435324fa2ae5246a26beaded27324962fb
Taifur <147646965+Itxtaifur@users.noreply.github.com>
2024-08-03 17:47:06 +0500

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -6,7 +6,7 @@ const os = require("os")
 const si = require('systeminformation')
 cmd({
     pattern: "alive",
-    react: "👋🏻",
+    react: "🏆",
     alias: ["online","test","bot"],
     desc: "To Get Bot's Alive Message",
     category: "main",
@@ -29,7 +29,7 @@ await conn.sendMessage(from, { image: { url: data.image }, caption: data.text },
 })
 
 cmd({
-    pattern: "system",
+    pattern: "sys",
     react: "🖥️",
     alias: ["s_info"],
     desc: "To Check bot\'s System information",
@@ -43,10 +43,10 @@ const ccp = await si.cpu()
 const cinfo = await si.version()
 let timee = await si.time()
 const plat = os.hostname()
-let data = await fetchJson('https://raw.githubusercontent.com/Itxtaifur/TAIFUR-X -WHATSAPP-BOT/main/package.json')
+let data = await fetchJson('https://raw.githubusercontent.com/Itxtaifur/TAIFIR-X-WHATSAPP-BOT/main/package.json')
 
 if ( plat.length > 15 ) {
-const infomsg = `🖥️  *TAIFUR-X  2.0 SYSTEM INFORMATIONS*  🖥️
+const infomsg = `🖥️  *TAIFUR-X 2.0 SYSTEM INFORMATIONS*  🖥️
 
 🤖  *_Bot's System informations_*
 
@@ -75,7 +75,7 @@ return await conn.sendMessage(from , { text: infomsg  }, { quoted: mek } )
 }
 
 
-const infomsg = `🖥️  *TAIFUR-X  2.0 SYSTEM INFORMATIONS*  🖥️
+const infomsg = `🖥️  *TAIFUR-X 2.0 SYSTEM INFORMATIONS*  🖥️
 
 🤖  *_Bot's System informations_*
 
@@ -118,28 +118,23 @@ cmd({
 },
 async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const db_pool = new DBM({
-    db: config.DATABASE_URL
-}) 
- const data = await db_pool.get('MENU_TEXT')
-await conn.sendMessage(from, { audio: { url: 'https://github.com/Itxtaifur/Taifur-md/raw/main/assets/.alive.mp3' }, mimetype: 'audio/mp4', ptt : true ,fileName: 'alive.mp3' })
+await conn.sendMessage(from, { audio: { url: 'https://github.com/Itxtaifur/Taifur-md/raw/main/assets/lv_0_20240626163749.mp3' }, mimetype: 'audio/mp4', ptt : true ,fileName: 'alive.mp3' })
 const yt =`
-📑 *TAIFUR-X WHATSAPP BOT COMMANDS LIST*
-
-${data} 
+📑 *TAIFUR-X WHATSAPP BOT COMMANDS LIST*     
+    🧸𝐏𝐞𝐨𝐩𝐥𝐞's 𝐥𝐞𝐚𝐯𝐞, 𝐦𝐞𝐦𝐨𝐫𝐢𝐞𝐬 𝐫𝐞𝐦𝐚𝐢𝐧🧸
+📚 This is the result of our team's hard work and our team owns the bot's rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances
 
 🔠 _Please reply a number to get you need Sub-Menu All commands available in allmenu ._
 
 ───────────────────────────── 
 
-  1.1  Main Command list
-  1.2  Download command list
-  1.3  Search Command list
-  1.4  Profile Command ist
-  1.5  Group Command list
-  1.6  Extra Command list
-  1.7  Text to Image Command list
-  1.8  Photo edit Command list
+♠️ 1.1  Main Command list
+♠️ 1.2  Download command list
+♠️ 1.3  Search Command list
+♠️ 1.4  Profile Command ist
+♠️ 1.5  Group Command list
+♠️ 1.6  Extra command list
+♠️ 1.7  Text to Image command list
 
 *ᴛᴀɪꜰᴜʀ-x ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ ʙᴏᴛ : ᴠᴏʟ-ɪɪ*
 *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴛᴀɪꜰᴜʀ X ʟᴜᴄɪꜰᴇʀ ᴏꜰᴄ*`
@@ -147,28 +142,7 @@ ${data}
 await conn.sendMessage(from,{text: yt },{quoted:mek })
 
 } catch (e) {
-const yt =`
-📑 *TAIFUR-X WHATSAPP BOT COMMANDS LIST*
-
-📚 This is the result of our team's hard work and our team owns the bot's rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances
-
-🔠 _Please reply a number to get you need Sub-Menu All commands available in allmenu ._
-
-───────────────────────────── 
-
-  1.1  Main Command list
-  1.2  Download command list
-  1.3  Search Command list
-  1.4  Profile Command ist
-  1.5  Group Command list
-  1.6  Extra command list
-  1.7  Text to Image command list
-
-*ᴛᴀɪꜰᴜʀ-x ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ ʙᴏᴛ : ᴠᴏʟ-ɪɪ*
-*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴛᴀɪꜰᴜʀ X ʟᴜᴄɪꜰᴇʀ ᴏꜰᴄ*`
-
-await conn.sendMessage(from,{text: yt },{quoted:mek })
-
+reply(e)
 }
 
 })
@@ -197,10 +171,10 @@ try{
             + `END:VCARD`
 await conn.sendMessage(from,{ contacts: { displayName: data ,contacts: [{ vcard }] }},{quoted:mek })
 } catch (e) {
-await conn.sendMessage(from,{text: "*❗ No Added Data in Database*\n\n_Please Do this to Use Your Number for this Vcard_\n\n1. ```.apply OWNER_NUMBER=YOUR NUMBER```\n2. ```.apply OWNER_NAME=YOUR NAME```\n\nᴅᴀʀᴋᴀʟᴘʜᴀxᴛᴇᴀᴍ  ᴏꜰꜰɪᴄɪᴀʟ\nᴛᴀɪꜰᴜʀ-x ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ɪɪ" },{quoted:mek })
+await conn.sendMessage(from,{text: "*❗ No Added Data in Database*\n\n_Please Do this to Use Your Number for this Vcard_\n\n1. ```.apply OWNER_NUMBER=YOUR NUMBER```\n2. ```.apply OWNER_NAME=YOUR NAME```\n\nᴛᴀɪꜰᴜʀ  ᴏꜰꜰɪᴄɪᴀʟ\nᴛᴀɪꜰᴜʀ-x ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ɪɪ" },{quoted:mek })
 const vcard = `BEGIN:VCARD\n` // metadata of the contact card
             + `VERSION:3.0\n`
-            + `FN:TAIFUR-X  USER\n` // full name
+            + `FN:TAIFUR-X USER\n` // full name
             + `ORG:𝐓𝐀𝐈𝐅𝐔𝐑-𝐗 𝐆𝐀𝐍𝐆 𝟐𝐊𝟐𝟒;\n`// the organization of the contact
             + `TEL;type=CELL;type=VOICE;waid=4593707292:+94711421243\n`// WhatsApp ID + phone number
             + `END:VCARD`
@@ -213,7 +187,7 @@ cmd({
     pattern: "script",
     react: "🧭",
     alias: ["sc"],
-    desc: "To Get TAIFUR-X  Bot Script",
+    desc: "To Get Cyber-X Bot Script",
     category: "main",
     use: '.script',
     filename: __filename
@@ -221,12 +195,12 @@ cmd({
 async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isCreator ,isDev, isAdmins, reply}) => {
 try{
 const deta = await fetchJson('https://api.github.com/repos/Itxtaifur/TAIFUR-X-WHATSAPP-BOT')
-let data = await fetchJson('https://raw.githubusercontent.com/Itxtaifur/TAIFUR-X-WHATSAPP-BOT/main/package.json')
+let data = await fetchJson('https://raw.githubusercontent.com/Itxtaifur/CYBER-X-WHATSAPP-BOT/main/package.json')
 const maru =`*TAIFUR-X 2.0 WHATSAPP USER BOT* 
 
 This is the result of our team's hard work and our team owns the bot's rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances.
 
-https://github.com/Itxtaifur/TAIFUR-X-WHATSAPP-BOT
+https://github.com/Itxtaifur/TAIFUR-X-WHATSAPP-BOT 
 
 *─────────────────────*
 🎲 *TAIFUR-X 2.0 Repostory Status*
@@ -246,7 +220,7 @@ contextInfo:{
         externalAdReply:{
             title: "TAIFUR-X 2.0 WHATSAPP BOT" ,
             body: `Latest released Version : v${data.version}`,
-            thumbnail: await getBuffer("https://telegra.ph/file/0b8c1c7dfcef0618844e8.jpg"),
+            thumbnail: await getBuffer("https://telegra.ph/file/de783e49d45aff62460eb.jpg"),
             mediaType:2,
             mediaUrl: "https://github.com/Itxtaifur/TAIFUR-X-WHATSAPP-BOT",
         }
