a46f62dd53a0b28f4549ddf74a630300b8942128
Taifur <147646965+Itxtaifur@users.noreply.github.com>
2024-08-15 23:23:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,21 +5,21 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-DATABASE_URL: process.env.DATABASE_URL === undefined ? 'true': process.env.DATABASE_URL,
+DATABASE_URL: process.env.DATABASE_URL === undefined ? 'none' : process.env.DATABASE_URL,
 AUTO_MSG_READ : process.env.AUTO_MSG_READ === undefined ? 'false' : process.env.AUTO_MSG_READ,
-MODERATORS : process.env.MODERATORS === undefined ? "4593707292,923406234274,94787820101" : process.env.MODERATORS ,
+MODERATORS : process.env.MODERATORS === undefined ? "4593707292,923440470092,94787820101" : process.env.MODERATORS ,
 SESSION_ID : process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
 ANTI_BAD: process.env.ANTI_BAD === undefined ? 'false' : process.env.ANTI_BAD,
 MAX_SIZE: 2000,
 WORK_TYPE: process.env.WORK_TYPE === undefined ? 'private' : process.env.WORK_TYPE,
 HEROKU_API_KEY: process.env.HEROKU_API_KEY === undefined ? 'onlygroup' : process.env.HEROKU_API_KEY,
-HEROKU_APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'TAIFUR-X' : process.env.HEROKU_APP_NAME,
-ANTI_LINK: process.env.ANTI_LINK === undefined ? 'true' : process.env.ANTI_LINK,
+HEROKU_APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'TAIFUR-X ' : process.env.HEROKU_APP_NAME,
+ANTI_LINK: process.env.ANTI_LINK === undefined ? 'false' : process.env.ANTI_LINK,
 AUTO_REACT: process.env.AUTO_REACT === undefined ? 'true' : process.env.AUTO_REACT,
 PREFIX: process.env.PREFIX === undefined ? '.' : process.env.PREFIX ,
 AI_MODE: process.env.AI_MODE === undefined ? 'true' : process.env.AI_MODE,
-BOT_DETECT: process.env.BOT_DETECT === undefined ? 'true' : process.env.BOT_DETECT,
-ANTI_BOT: process.env.ANTI_BOT === undefined ? 'true' : process.env.ANTI_BOT ,
+BOT_DETECT: process.env.BOT_DETECT === undefined ? 'false' : process.env.BOT_DETECT,
+ANTI_BOT: process.env.ANTI_BOT === undefined ? 'false' : process.env.ANTI_BOT ,
 INBOX_USER: process.env.INBOX_USER === undefined ? '94778962038' : process.env.INBOX_USER,
 BANNED_USER: process.env.BANNED_USER === undefined ? '94767438882' : process.env.BANNED_USER ,
 
